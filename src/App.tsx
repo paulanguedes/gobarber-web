e0afddce570dffa14575f233f81f7996f4c3ef69
paulanguedes <paulanguedes@gmail.com>
2021-03-11 20:38:11 -0300

    log in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn/index';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Helloo, World!</h1>
+      <SignIn />
       <GlobalStyle />
     </>
   );
