a90d029148ccad5d292a1428babbfa6f63dab724
paulanguedes <paulanguedes@gmail.com>
2021-03-12 19:09:40 -0300

    sign up component]

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input/index';
+import Button from '../../components/Button/index';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+      <form>
+        <h1>Faça seu Cadastro</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="/login">
+        <FiArrowLeft />
+        Voltar
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
