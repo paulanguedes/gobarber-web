18a38d14080cb291b79bf551faa384fe7064bd99
paulanguedes <paulanguedes@gmail.com>
2021-05-02 16:42:57 -0300

    :construction: build -> dashboard's calendar

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -20,6 +24,13 @@ import {
 
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = useCallback(
+    (day: Date, modifiers: DayModifiers) => {
+      if (modifiers.available) {
+        setSelectedDate(day);
+      }
+    }, []);
 
   const { signOut, user } = useAuth();
 
@@ -66,63 +77,86 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/60984558?v=4"
+                  alt="Paula Guedes"
+                />
+                <strong>Paula Guedes</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/60984558?v=4"
+                  alt="Paula Guedes"
+                />
+                <strong>Paula Guedes</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/60984558?v=4"
+                  alt="Paula Guedes"
+                />
+                <strong>Paula Guedes</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
 
-        <Section>
-          <strong>Manhã</strong>
-
-          <Appointment>
-            <span>
-              <FiClock />
-              08:00
-            </span>
-
-            <div>
-              <img
-                src="https://avatars.githubusercontent.com/u/60984558?v=4"
-                alt="Paula Guedes"
-              />
-              <strong>Paula Guedes</strong>
-            </div>
-          </Appointment>
-        </Section>
-
-        <Section>
-          <strong>Tarde</strong>
-
-          <Appointment>
-            <span>
-              <FiClock />
-              08:00
-            </span>
-
-            <div>
-              <img
-                src="https://avatars.githubusercontent.com/u/60984558?v=4"
-                alt="Paula Guedes"
-              />
-              <strong>Paula Guedes</strong>
-            </div>
-          </Appointment>
-
-          <Appointment>
-            <span>
-              <FiClock />
-              08:00
-            </span>
-
-            <div>
-              <img
-                src="https://avatars.githubusercontent.com/u/60984558?v=4"
-                alt="Paula Guedes"
-              />
-              <strong>Paula Guedes</strong>
-            </div>
-          </Appointment>
-        </Section>
-
         <Calendar>
-          <h6>calendar</h6>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            months={[
+              'Janeiro',
+              'Feveiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
         </Calendar>
       </Content>
     </Container>
